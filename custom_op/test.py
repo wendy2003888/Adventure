536dad3d03dcc069c5c1c6d87623393232773777
wendyh <wendyh2003888@gmail.com>
2019-02-26 23:07:33 -0800

    clean

--- custom_op/test.py
+++ custom_op/test.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 test_op = tf.load_op_library('./test_op.so')
+# test_op = tf.load_op_library('bazel-bin/tensorflow/custom_op/test_op.so')
+
+
 with tf.Session('') as sess:
   # test_op.example([[1, 2], [3, 4]]).eval()
   print (sess.run(test_op.example([[1, 2], [3, 4]])))
